ffa79b88ea077b865b387802b5f614a2a1bdd71c
KmPostres <monseparker856@gmail.com>
2026-09-13 15:54:04 -0600

    Primer commit - proyecto conectado a MySQL

---
+++ backend/app/probar_conexion.py
@@ -0,0 +1,12 @@
+from backend.config.conexion import conectar
+conexion = conectar()
+if conexion:
+  print("==============================")
+  print("CONEXIÓN EXITOSA")
+  print("Base de datos: tecnico")
+  print("==============================")
+  conexion.close()
+else:
+  print("==============================")
+  print("ERROR DE CONEXIÓN")
+  print("==============================")
